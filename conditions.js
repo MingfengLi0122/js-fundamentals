6c80c3689512fb90d80b3979a6611f05147cd2a7
Mingfeng Li <lmf950122@gmail.com>
2021-05-11 22:39:25 +0000

    Add more conditional examples

---
+++ conditions.js
@@ -0,0 +1,7 @@
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18){
+	console.log("You are eligible to vote");
+}
+
---
+++ rain.js
@@ -0,0 +1,7 @@
+const raining = true;
+
+if(raining){
+	console.log("Please take your umbrella");
+}else{
+	console.log("Leave your umbrella at home");
+}
